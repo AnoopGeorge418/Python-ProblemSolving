225597d957cad9baac88a4c1bb916934dbfc4ca9
Anoop George <anoopgeorge418@gmail.com>
2025-02-24 20:38:53 +0530

    Largest number finder

---
+++ 4. findLargest.py
@@ -0,0 +1,12 @@
+# finding the largest number from user input
+
+# step get inputs from user
+# find the largest input from it
+
+num = input('Enter the numbers (use , to add more numbers): ')
+
+# numbers = []
+# for i in num:
+#     numbers += i
+
+print(f'Largest number from {num} is: {max(num)}')
